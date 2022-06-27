757b0d19548a7f3f91c0e9c959c964b5fd4adbe6
Mwadime <brian.mwadime@gmail.com>
2022-06-27 22:44:40 +0300

    Update systango_hrm_controller.rb

--- app/controllers/systango_hrm_controller.rb
+++ app/controllers/systango_hrm_controller.rb
@@ -1,4 +1,4 @@
 class SystangoHrmController < ApplicationController
-  before_filter :require_login, :authorize_global
+  before_action :require_login, :authorize_global
   skip_before_filter :authorize_global, :only => [:calendar]
 end
