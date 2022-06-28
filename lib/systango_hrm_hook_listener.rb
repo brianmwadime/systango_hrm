1ef2f85500fc81ff18d5eefd12cf1eb2d64b4feb
Mwadime <brian.mwadime@gmail.com>
2022-06-28 15:49:12 +0300

    Update systango_hrm_hook_listener.rb

--- lib/systango_hrm_hook_listener.rb
+++ lib/systango_hrm_hook_listener.rb
@@ -1,6 +1,6 @@
-module SystangoHrm
+
 	class SystangoHrmHookListener < Redmine::Hook::ViewListener
 		render_on :view_welcome_index_right, :partial => "systango_hrm_employee_leaves/my_page"
   		render_on :view_welcome_index_left, :partial => "systango_hrm_employee_leaves/hr_hook"
 	end
-end
+
